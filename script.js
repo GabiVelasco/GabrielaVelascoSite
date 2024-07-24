93376dcc82f2604862be5d40d29d68ff32337612
Gabriela Velasco <gabriela.luppe@gmail.com>
2024-07-24 23:44:41 +0200

    slides - fixed

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-let slideIndex = 0;
+let slideIndex = 1;
 let timer;
 
 // Initial call to start the slideshow
-showSlides();
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
+    
+    // If n is greater than the number of slides, reset to 1
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    
+    // If n is less than 1, set it to the last slide
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     
     // Hide all slides
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
-    }
-    
-    // Increment slideIndex
-    slideIndex++;
-    
-    // If slideIndex is greater than the number of slides, reset to 1
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
-    // If slideIndex is less than 1, set it to the last slide
-    if (slideIndex < 1) {
-        slideIndex = slides.length;
     }
     
     // Display the current slide
@@ -30,7 +27,10 @@ function showSlides() {
     
     // Clear previous timer and set a new one
     clearTimeout(timer);
-    timer = setTimeout(showSlides, 5000); // Change slide every 5 seconds
+    timer = setTimeout(() => {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 5000); // Change slide every 5 seconds
 }
 
 function plusSlides(n) {
@@ -42,6 +42,7 @@ function minusSlides() {
     clearTimeout(timer); // Clear the timer when navigating manually
     showSlides(slideIndex -= 1);
 }
+
 
 
 
